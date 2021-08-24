0014f9b0c0ec409cd03b793c23a003b50a2af809
michael gastal <michaelgastal@gmail.com>
2021-08-24 13:55:39 +0200

    add Gris in the eyes array

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   HAIR = ["Blond", "Brun", "Châtain", "Roux", "Gris", "Blanc"]
-  EYES = ["Bleu", "Vert", "Marron"]
+  EYES = ["Bleu", "Vert", "Marron", "Gris"]
 
   validates :first_name, presence: true, length: { minimum: 3 }
   validates :last_name, presence: true, length: { minimum: 2 }
