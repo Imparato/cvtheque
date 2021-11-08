8dfd7b2421b0deca39427184126ec7a1caaf52b8
michael gastal <michaelgastal@gmail.com>
2021-11-08 20:41:26 +0100

    :construction: Ajout des champs postal_code et city à la table users / Focus border green sur les champs du formulaire / Traduction en français pour la connexion et la création de compte / Tous les champs dans le formulaire pour la création d'un compte

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,15 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :phone_number, presence: true, format: { with: /\A0\d( *\d{2}){4}/ }
   validates :address, presence: true
+  validates :postal_code, presence: true
+  validates :city, presence: true
   validates :birthday, presence: true
   validates :hair_color, inclusion: { in: HAIR }
   validates :eyes_color, inclusion: { in: EYES }
   validates :size, numericality: { only_integer: true }
   validates :weight, numericality: { only_integer: true }
-  validates :linkedin_url, format: { with: /\Ahttps:\/\/www.linkedin.com\/in\// }
-  validates :facebook_url, format: { with: /\Ahttps:\/\/www.facebook.com\// }
-  validates :twitter_url, format: { with: /\Ahttps:\/\/www.twitter.com\// }
-  validates :instagram_url, format: { with: /\Ahttps:\/\/www.instagram.com\// }
+  validates :linkedin_url, format: { with: %r{\Ahttps://www.linkedin.com/in/} }
+  validates :facebook_url, format: { with: %r{\Ahttps://www.facebook.com/} }
+  validates :twitter_url, format: { with: %r{\Ahttps://www.twitter.com/} }
+  validates :instagram_url, format: { with: %r{\Ahttps://www.instagram.com/} }
 end
